663bdc45309afff49cc04b9dd7cb2c12b45ac9aa
Michael Grubb <michael.grubb@acxiom.com>
2017-02-01 16:08:21 -0600

    problem setup

---
+++ cpp/word-count/word_count.cpp
@@ -0,0 +1,10 @@
+#include "word_count.h"
+
+using namespace std;
+
+map<string, int> word_count::words(const char *)
+{
+  map<string, int> &res = *(new map<string,int>);
+
+  return res;
+}
---
+++ cpp/word-count/word_count.h
@@ -0,0 +1,11 @@
+#ifndef _WORD_COUNT_H_
+#define _WORD_COUNT_H_
+
+#include <string>
+#include <map>
+
+namespace word_count
+{
+  std::map<std::string, int> words(const char *);
+};
+#endif /* _WORD_COUNT_H_ */
