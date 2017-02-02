4eb2eb8906d2f09e11576bc92aa20ba27a799575
Michael Grubb <michael@grubb.co>
2017-02-02 10:57:42 -0600

    cpp word-count

--- cpp/word-count/word_count.cpp
+++ cpp/word-count/word_count.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <iterator>
+#include <boost/regex.hpp>
 #include "word_count.h"
 
 using namespace std;
@@ -22,38 +23,23 @@ namespace word_count
     return s.empty();
   }
 
-  static vector<string> tokenize(const string phrase, const string delim = whitespace)
+  static vector<string> tokenize(const string phrase)
   {
-    string str;
-    vector<string> &res = *(new vector<string>());
-    typedef string::const_iterator sci;
-    string *rs;
-    sci strb, stre, db, de, r;
+    vector<string> &res = *(new vector<string>);
+    boost::regex pattern {"[a-zA-Z0-9']+"};
+    boost::regex quote {"(^'|'$)"};
+    boost::sregex_iterator it(phrase.begin(), phrase.end(), pattern);
+    boost::sregex_iterator end;
 
-    copy_if(phrase.begin(), phrase.end(), back_inserter(str), keep_char);
-    transform(str.begin(), str.end(), str.begin(), ::tolower);
-    strb = str.begin();
-    stre = str.end();
-    db = delim.begin();
-    de = delim.end();
-
-    r = find_first_of(strb, stre, db, de);
-    while (r != stre)
-                {
-                  rs = new string(strb, r);
-                  res.push_back(*rs);
-                  strb = r+1;
-                  r = find_first_of(strb, stre, db, de);
-                } 
-
-    if (res.empty())
-      rs = new string(str);
-    else
-      rs = new string(strb, stre);
-
-    res.push_back(*rs);
-    res.erase(remove_if(res.begin(), res.end(), word_count::is_empty),
-              res.end());
+    for (; it != end; ++it)
+    {
+      string s = boost::regex_replace(it->str(), quote, "");
+      transform(s.begin(), s.end(), s.begin(), ::tolower);
+      if (s.length() > 0)
+      {
+        res.push_back(s);
+      }
+    }
     return res;
   }
 
